b61b22fa637937b200d8a3c24f6e49842dee9bb2
Srinivas-2906 <srinivas60.yerala@gmail.com>
2025-03-20 12:39:18 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-base: mode === 'production' ? "/InfluncerProject/" : "/",
+  base: mode === 'production' ? "/InfluncerProject/" : "/",
   server: {
     host: "::",
     port: 8080,
